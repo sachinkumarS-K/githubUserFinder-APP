accc68e54e2c12b43b87545ad2dccbfdddca1934
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2023-10-27 14:21:16 +0530

    error fixed

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './Search.css'
 import { useDebounce } from '../../hooks/useDebounce'
 function Search({ serchTerm, setSeachTerm }) {
-  // const debounceCallback = useDebounce((e) => setSeachTerm(e.target.value))
+   const debounceCallback = useDebounce((e) => setSeachTerm(e.target.value))
   function changeHandler(e) {
     console.log(e.target.value)
     setSeachTerm(e.target.value)
   }
   return (
     <div className='inpCont'>
-      <input type="text" onChange={changeHandler} placeholder='Enter photo Id' id="" />
+      <input type="text" onChange={debounceCallback} placeholder='Enter photo Id' id="" />
     </div>
   )
 }
